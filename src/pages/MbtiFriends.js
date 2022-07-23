2718de4907f61d710cbb5e4925ca540276c163b4
ssjeu <sje1015s@gmail.com>
2022-07-23 23:59:59 +0900

    [feat]

---
+++ src/pages/MbtiFriends.js
@@ -0,0 +1,64 @@
+// 다양한 MBTI 친구들
+import React, { useEffect } from "react";
+import styled from "styled-components";
+import { useNavigate } from "react-router-dom";
+import { useDispatch, useSelector } from "react-redux";
+import { actionCreators as recommendActions } from "../redux/modules/recommend";
+
+import "../css/component.css";
+import FilterIcon from "../images/icons/filter-alt@3x.png";
+
+const MbtiFriends = () => {
+  const dispatch = useDispatch();
+  const navigate = useNavigate();
+
+  // 다양한 MBTI 친구들
+  const users = useSelector((state) => state.recommend.mbti);
+  console.log(users);
+  
+  useEffect(() => {
+    dispatch(recommendActions.mbtiFriendDB());
+  }, []);
+
+  return (
+    <MbtiFriendsWrap>
+      <Top className="contents-container">
+        다양한 MBTI 친구들
+        <FilterBtn onClick={() => navigate("/mbtifilter")}>
+          <img src={FilterIcon} alt="MBTI filter" />
+          필터
+        </FilterBtn>
+      </Top>
+    </MbtiFriendsWrap>
+  );
+};
+
+const MbtiFriendsWrap = styled.div``;
+
+const Top = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  font-size: 16px;
+  font-weight: 500;
+  letter-spacing: -0.8px;
+`;
+
+const FilterBtn = styled.div`
+  width: 88px;
+  height: 32px;
+  background-color: var(--maincolor);
+  border-radius: 16px;
+  color: white;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+
+  & img {
+    width: 16px;
+    height: 16px;
+    margin-right: 4px;
+  }
+`;
+
+export default MbtiFriends;
